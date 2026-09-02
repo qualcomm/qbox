75624cfa0a73039ccce9283aad3e1b0738e2d691
Mahmoud Kamel <mkamel@qti.qualcomm.com>
2026-09-02 10:42:48 +0200

    systemc-components/addrtr: fix an issue in the map_dmi_start_end_addr function
    - The end_addr in the function map_dmi_start_end_addr() which is used to transform
    the get_direct_mem_ptr was checked wrongly against the modified start_addr, check
    it correctly against p_mapped_base_addr.
    - Modify the test bench to accomodate the change.
    
    Signed-off-by: Mahmoud Kamel <mkamel@qti.qualcomm.com>

--- tests/components/addrtr/addrtr-tests.cc
+++ tests/components/addrtr/addrtr-tests.cc
@@ -16,18 +16,32 @@
  */
 TEST_BENCH(AddrtrTestBench, Tester)
 {
-    do_txn(0x100ULL, 0);
-    do_txn(0x100ULL, 1);
-    do_dmi(0x100ULL);
+    do_txn(TARGET_BASE_ADDR, 0);
+    do_txn(TARGET_BASE_ADDR, 1);
+    do_dmi(TARGET_BASE_ADDR);
 }
+
+TEST_BENCH(AddrtrTestBench, DmiRange)
+{
+    const auto addr = TARGET_BASE_ADDR + AddrtrTestBench::DMI_RANGE_OFFSET;
+    do_dmi_with_range(addr, AddrtrTestBench::DMI_RANGE_SIZE);
+}
+
+TEST_BENCH(AddrtrTestBench, DmiBoundary) { do_dmi_with_range(TARGET_BASE_ADDR, AddrtrTestBench::TARGET_MMIO_SIZE); }
 
 int sc_main(int argc, char* argv[])
 {
     gs::ConfigurableBroker m_broker({
         { "log_level", cci::cci_value(5) },
-        { "Tester.exclusive_addrtr.target_socket.address", cci::cci_value(0x100ULL) },
+        { "Tester.exclusive_addrtr.target_socket.address", cci::cci_value(AddrtrTestBench::TARGET_BASE_ADDR) },
         { "Tester.exclusive_addrtr.target_socket.size", cci::cci_value(AddrtrTestBench::TARGET_MMIO_SIZE) },
-        { "Tester.exclusive_addrtr.mapped_base_addr", cci::cci_value(0x110ULL) },
+        { "Tester.exclusive_addrtr.mapped_base_addr", cci::cci_value(AddrtrTestBench::MAPPED_BASE_ADDR) },
+        { "DmiRange.exclusive_addrtr.target_socket.address", cci::cci_value(AddrtrTestBench::TARGET_BASE_ADDR) },
+        { "DmiRange.exclusive_addrtr.target_socket.size", cci::cci_value(AddrtrTestBench::TARGET_MMIO_SIZE) },
+        { "DmiRange.exclusive_addrtr.mapped_base_addr", cci::cci_value(AddrtrTestBench::MAPPED_BASE_ADDR) },
+        { "DmiBoundary.exclusive_addrtr.target_socket.address", cci::cci_value(AddrtrTestBench::TARGET_BASE_ADDR) },
+        { "DmiBoundary.exclusive_addrtr.target_socket.size", cci::cci_value(AddrtrTestBench::TARGET_MMIO_SIZE) },
+        { "DmiBoundary.exclusive_addrtr.mapped_base_addr", cci::cci_value(AddrtrTestBench::MAPPED_BASE_ADDR) },
     });
 
     ::testing::InitGoogleTest(&argc, argv);
