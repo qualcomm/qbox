84faaef9ca5f6946ec8a3bb05193124aab0f4763
Mahmoud Kamel <mkamel@qti.qualcomm.com>
2026-09-11 11:08:41 +0200

    systemc-components/addrtr: Fix addrtr DMI pointer mapping issue
    - The dmi pointer was not mapped correctly before, so add the correct maping and
    preserve accurate DMI start/end addresses with the corresponding pointer offset when ranges
    are clipped.
    
    Signed-off-by: Mahmoud Kamel <mkamel@qti.qualcomm.com>

--- tests/components/addrtr/addrtr-bench.h
+++ tests/components/addrtr/addrtr-bench.h
@@ -8,6 +8,8 @@
 #define _BASE_COMPONENTS_TESTS_ADDRTR_TEST_BENCH_H
 
 #include <functional>
+#include <array>
+#include <optional>
 
 #include <systemc>
 #include <tlm>
@@ -26,6 +28,9 @@ public:
     static constexpr uint64_t MAPPED_BASE_ADDR = 0x110;
     static constexpr uint64_t DMI_RANGE_OFFSET = 0x20;
     static constexpr uint64_t DMI_RANGE_SIZE = 0x100;
+    static constexpr uint64_t TARGET_END_ADDR = TARGET_BASE_ADDR + TARGET_MMIO_SIZE - 1;
+    static constexpr uint64_t MAPPED_END_ADDR = MAPPED_BASE_ADDR + TARGET_MMIO_SIZE - 1;
+    static constexpr uint64_t DOWNSTREAM_TESTER_SIZE = MAPPED_BASE_ADDR + TARGET_MMIO_SIZE + 16;
 
     class TargetTesterDMIinv : public TargetTester
     {
@@ -48,15 +53,26 @@ private:
 
     uint64_t sent_addr = 0;
     bool return_dmi_range = false;
+    bool return_dmi_from_zero = false;
+    bool dmi_return_value = true;
+    bool use_explicit_dmi_range = false;
     uint64_t dmi_range_size = 0;
+    uint64_t explicit_dmi_start = 0;
+    uint64_t explicit_dmi_end = 0;
+    uint64_t explicit_dmi_ptr_offset = 0;
+    std::array<unsigned char, MAPPED_BASE_ADDR + TARGET_MMIO_SIZE> dmi_memory{};
+    std::optional<std::pair<uint64_t, uint64_t>> expected_invalidation;
+    unsigned int invalidation_count = 0;
 
     uint64_t mapped_addr() const { return MAPPED_BASE_ADDR + (sent_addr - TARGET_BASE_ADDR); }
 
     /* Initiator callback */
     void invalidate_direct_mem_ptr(uint64_t start_range, uint64_t end_range)
     {
-        EXPECT_EQ(start_range, TARGET_BASE_ADDR);
-        EXPECT_EQ(end_range, TARGET_BASE_ADDR + TARGET_MMIO_SIZE - 1);
+        invalidation_count++;
+        ASSERT_TRUE(expected_invalidation.has_value());
+        EXPECT_EQ(start_range, expected_invalidation->first);
+        EXPECT_EQ(end_range, expected_invalidation->second);
     }
 
     /* Target callbacks */
@@ -69,9 +85,22 @@ private:
     bool get_direct_mem_ptr(uint64_t addr, TlmDmi& dmi_data)
     {
         EXPECT_EQ(addr, mapped_addr());
+        if (!dmi_return_value) {
+            return false;
+        }
         dmi_data.allow_read_write();
-        dmi_data.set_start_address(addr);
-        dmi_data.set_end_address(return_dmi_range ? addr + dmi_range_size - 1 : addr);
+        if (use_explicit_dmi_range) {
+            dmi_data.set_dmi_ptr(dmi_memory.data() + explicit_dmi_ptr_offset);
+            dmi_data.set_start_address(explicit_dmi_start);
+            dmi_data.set_end_address(explicit_dmi_end);
+        } else if (return_dmi_from_zero) {
+            dmi_data.set_dmi_ptr(dmi_memory.data());
+            dmi_data.set_start_address(0);
+            dmi_data.set_end_address(MAPPED_END_ADDR);
+        } else {
+            dmi_data.set_start_address(addr);
+            dmi_data.set_end_address(return_dmi_range ? addr + dmi_range_size - 1 : addr);
+        }
         return true;
     }
 
@@ -81,6 +110,7 @@ protected:
         uint64_t data = 0x42ULL;
         TlmGenericPayload txn;
         sent_addr = addr;
+        dmi_return_value = true;
         if (!dbg) {
             ASSERT_EQ(tlm::TLM_OK_RESPONSE, m_initiator.do_write(addr, data, dbg));
         } else {
@@ -88,22 +118,43 @@ protected:
         }
     }
 
+    void do_txn_with_len(uint64_t addr, size_t len)
+    {
+        std::array<uint8_t, 16> data{};
+        TlmGenericPayload txn;
+
+        ASSERT_LE(len, data.size());
+        sent_addr = addr;
+        dmi_return_value = true;
+        ASSERT_EQ(tlm::TLM_OK_RESPONSE, m_initiator.do_write_with_txn_and_ptr(txn, addr, data.data(), len));
+        EXPECT_EQ(txn.get_address(), addr);
+    }
+
     void do_dmi(uint64_t addr)
     {
         sent_addr = addr;
+        dmi_return_value = true;
+        use_explicit_dmi_range = false;
         return_dmi_range = false;
+        return_dmi_from_zero = false;
         dmi_range_size = 0;
         ASSERT_TRUE(m_initiator.do_dmi_request(addr));
         const auto& dmi = m_initiator.get_last_dmi_data();
         ASSERT_EQ(dmi.get_start_address(), sent_addr);
         ASSERT_EQ(dmi.get_end_address(), sent_addr);
-        m_target.do_dmi_invalidate(MAPPED_BASE_ADDR, MAPPED_BASE_ADDR + TARGET_MMIO_SIZE - 1);
+        expected_invalidation = std::make_pair(TARGET_BASE_ADDR, TARGET_END_ADDR);
+        invalidation_count = 0;
+        m_target.do_dmi_invalidate(MAPPED_BASE_ADDR, MAPPED_END_ADDR);
+        EXPECT_EQ(invalidation_count, 1u);
     }
 
     void do_dmi_with_range(uint64_t addr, uint64_t range_size)
     {
         sent_addr = addr;
+        dmi_return_value = true;
+        use_explicit_dmi_range = false;
         return_dmi_range = true;
+        return_dmi_from_zero = false;
         dmi_range_size = range_size;
         ASSERT_TRUE(m_initiator.do_dmi_request(addr));
         const auto& dmi = m_initiator.get_last_dmi_data();
@@ -111,12 +162,87 @@ protected:
         ASSERT_EQ(dmi.get_end_address(), sent_addr + range_size - 1);
     }
 
+    void do_dmi_with_clipped_downstream_start()
+    {
+        sent_addr = TARGET_BASE_ADDR;
+        dmi_return_value = true;
+        use_explicit_dmi_range = false;
+        return_dmi_range = false;
+        return_dmi_from_zero = true;
+        ASSERT_TRUE(m_initiator.do_dmi_request(sent_addr));
+        const auto& dmi = m_initiator.get_last_dmi_data();
+        ASSERT_EQ(dmi.get_start_address(), TARGET_BASE_ADDR);
+        ASSERT_EQ(dmi.get_end_address(), TARGET_END_ADDR);
+        ASSERT_EQ(dmi.get_dmi_ptr(), dmi_memory.data() + MAPPED_BASE_ADDR);
+    }
+
+    void do_dmi_with_explicit_range(uint64_t addr, uint64_t downstream_start, uint64_t downstream_end,
+                                    uint64_t ptr_offset, uint64_t expected_start, uint64_t expected_end,
+                                    uint64_t expected_ptr_offset)
+    {
+        sent_addr = addr;
+        dmi_return_value = true;
+        use_explicit_dmi_range = true;
+        return_dmi_range = false;
+        return_dmi_from_zero = false;
+        explicit_dmi_start = downstream_start;
+        explicit_dmi_end = downstream_end;
+        explicit_dmi_ptr_offset = ptr_offset;
+
+        ASSERT_TRUE(m_initiator.do_dmi_request(addr));
+        const auto& dmi = m_initiator.get_last_dmi_data();
+        ASSERT_EQ(dmi.get_start_address(), expected_start);
+        ASSERT_EQ(dmi.get_end_address(), expected_end);
+        ASSERT_EQ(dmi.get_dmi_ptr(), dmi_memory.data() + expected_ptr_offset);
+    }
+
+    void do_dmi_denied(uint64_t addr)
+    {
+        sent_addr = addr;
+        dmi_return_value = false;
+        use_explicit_dmi_range = false;
+        return_dmi_range = false;
+        return_dmi_from_zero = false;
+        ASSERT_FALSE(m_initiator.do_dmi_request(addr));
+    }
+
+    void do_dmi_non_overlapping_grant_returns_false(uint64_t addr, uint64_t downstream_start, uint64_t downstream_end)
+    {
+        sent_addr = addr;
+        dmi_return_value = true;
+        use_explicit_dmi_range = true;
+        return_dmi_range = false;
+        return_dmi_from_zero = false;
+        explicit_dmi_start = downstream_start;
+        explicit_dmi_end = downstream_end;
+        explicit_dmi_ptr_offset = 0;
+        ASSERT_FALSE(m_initiator.do_dmi_request(addr));
+    }
+
+    void do_invalidate(uint64_t downstream_start, uint64_t downstream_end, uint64_t expected_start,
+                       uint64_t expected_end)
+    {
+        expected_invalidation = std::make_pair(expected_start, expected_end);
+        invalidation_count = 0;
+        m_target.do_dmi_invalidate(downstream_start, downstream_end);
+        EXPECT_EQ(invalidation_count, 1u);
+        expected_invalidation.reset();
+    }
+
+    void do_non_overlapping_invalidate(uint64_t downstream_start, uint64_t downstream_end)
+    {
+        expected_invalidation.reset();
+        invalidation_count = 0;
+        m_target.do_dmi_invalidate(downstream_start, downstream_end);
+        EXPECT_EQ(invalidation_count, 0u);
+    }
+
 public:
     AddrtrTestBench(const sc_core::sc_module_name& n)
         : TestBench(n)
         , m_addrtr("exclusive_addrtr")
         , m_initiator("initiator-tester")
-        , m_target("target-tester", TARGET_MMIO_SIZE)
+        , m_target("target-tester", DOWNSTREAM_TESTER_SIZE)
     {
         using namespace std::placeholders;
 
